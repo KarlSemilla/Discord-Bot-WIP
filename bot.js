c7d77eaaa30e62d6e39c2d3856db4fa48a59650f
Karl <karlsemilla1@gmail.com>
2018-04-13 00:30:17 -0600

    Added ping command

--- bot.js
+++ bot.js
@@ -1,3 +1,9 @@
+/* Ark's Discord Bot
+*  Creator: @Arkanon 
+*  Date Created: April 12, 2018
+*  Last Updated: April 13, 2018
+*/
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./auth.json");
@@ -10,10 +16,24 @@ client.on("ready", () => {
 
 //This will prompt advice when a user types '!advice'.
 client.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.content.indexOf(config.prefix) !== 0) return;
+    //Checks if message is sent by bot vs. user.
+    if(message.author.bot) 
+        return;
     
-    const args = message.content.slice
+    //Checks if prefix is at index 0 and is same in config.
+    if(message.content.indexOf(auth.prefix) !== 0) 
+        return;
+    
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    
+    if(command === "ping") {
+        // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+        // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+        const m = await message.channel.send("Ping?");
+        m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+        }
+
 });
 
 
